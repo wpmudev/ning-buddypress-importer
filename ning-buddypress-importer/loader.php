99fae08cf1feaaa7d218de54210159b4c0deb6f2
aaron <aaron@f53fe46e-8c02-459b-9fdd-93e75771b278>
2011-09-29 22:37:23 +0000

    - BuddyPress 1.5 Compatibility fix
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/ning-buddypress-importer@4197 f53fe46e-8c02-459b-9fdd-93e75771b278

--- ning-buddypress-importer/loader.php
+++ ning-buddypress-importer/loader.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Ning To BuddyPress User Importer
-Version: 1.0.4
+Version: 1.0.5
 Plugin URI: http://premium.wpmudev.org/project/ning-to-buddypress-user-importer/
 Description: Allows you to do a full import of a Ning network's users, their custom profile fields, and avatars to BuddyPress. Full support for very large member lists via optional FTP upload and batch processing.
 Author: Aaron Edwards (Incsub)
@@ -28,7 +28,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 function nbi_init() {
   require( dirname( __FILE__ ) . '/includes/ning-buddypress-importer.php' );
 }
-add_action( 'bp_init', 'nbi_init' );
+add_action( 'bp_include', 'nbi_init' );
 
 
 ///////////////////////////////////////////////////////////////////////////
@@ -37,7 +37,7 @@ if ( !function_exists( 'wdp_un_check' ) ) {
   add_action( 'admin_notices', 'wdp_un_check', 5 );
   add_action( 'network_admin_notices', 'wdp_un_check', 5 );
   function wdp_un_check() {
-    if ( !class_exists( 'WPMUDEV_Update_Notifications' ) && current_user_can( 'edit_users' ) )
+    if ( !class_exists( 'WPMUDEV_Update_Notifications' ) && current_user_can( 'install_plugins' ) )
       echo '<div class="error fade"><p>' . __('Please install the latest version of <a href="http://premium.wpmudev.org/project/update-notifications/" title="Download Now &raquo;">our free Update Notifications plugin</a> which helps you stay up-to-date with the most stable, secure versions of WPMU DEV themes and plugins. <a href="http://premium.wpmudev.org/wpmu-dev/update-notifications-plugin-information/">More information &raquo;</a>', 'wpmudev') . '</a></p></div>';
   }
 }
